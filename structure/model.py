63e8e8089618f69d4958c5aca41f011f865f6871
ChingHua <chinghua.yang@appier.com>
2022-06-29 00:17:16 +0800

    update model calling

--- structure/model.py
+++ structure/model.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 import backbones
 import decoders
 
+from .my_model import BasicModel as MyBasicModel
+
 
 class BasicModel(nn.Module):
     def __init__(self, args):
@@ -35,6 +37,43 @@ class SegDetectorModel(nn.Module):
         from decoders.seg_detector_loss import SegDetectorLossBuilder
 
         self.model = BasicModel(args)
+        # for loading models
+        self.model = parallelize(self.model, distributed, local_rank)
+        self.criterion = SegDetectorLossBuilder(
+            args['loss_class'], *args.get('loss_args', []), **args.get('loss_kwargs', {})).build()
+        self.criterion = parallelize(self.criterion, distributed, local_rank)
+        self.device = device
+        self.to(self.device)
+
+    @staticmethod
+    def model_name(args):
+        return os.path.join('seg_detector', args['backbone'], args['loss_class'])
+
+    def forward(self, batch, training=True):
+        if isinstance(batch, dict):
+            data = batch['image'].to(self.device)
+        else:
+            data = batch.to(self.device)
+        data = data.float()
+        pred = self.model(data, training=self.training)
+
+        if self.training:
+            for key, value in batch.items():
+                if value is not None:
+                    if hasattr(value, 'to'):
+                        batch[key] = value.to(self.device)
+            loss_with_metrics = self.criterion(pred, batch)
+            loss, metrics = loss_with_metrics
+            return loss, pred, metrics
+        return pred
+
+
+class MYSegDetectorModel(nn.Module):
+    def __init__(self, args, device, distributed: bool = False, local_rank: int = 0):
+        super(SegDetectorModel, self).__init__()
+        from decoders.seg_detector_loss import SegDetectorLossBuilder
+
+        self.model = MyBasicModel()
         # for loading models
         self.model = parallelize(self.model, distributed, local_rank)
         self.criterion = SegDetectorLossBuilder(
---
+++ structure/my_model.py
@@ -0,0 +1,46 @@
+import torch.nn as nn
+
+from torchvision.models.resnet import ResNet
+from torchvision.models.resnet import BasicBlock
+from decoders import SegDetector
+
+
+# ref: https://pytorch.org/vision/main/_modules/torchvision/models/resnet.html#resnet18
+def _resnet(
+    block,
+    layers,
+    weights,
+    progress,
+    **kwargs,
+):
+    model = ResNet18FPN(block, layers, **kwargs)
+
+    if weights is not None:
+        model.load_state_dict(weights.get_state_dict(progress=progress))
+
+    return model
+
+
+class ResNet18FPN(ResNet):
+    def forward_fpn(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x2 = self.layer1(x)
+        x3 = self.layer2(x2)
+        x4 = self.layer3(x3)
+        x5 = self.layer4(x4)
+
+        return x2, x3, x4, x5
+
+
+class BasicModel(nn.Module):
+    def __init__(self):
+        nn.Module.__init__(self)
+        self.backbone = _resnet(BasicBlock, [2, 2, 2, 2], progress=True)
+        self.decoder = SegDetector(in_channels=[64, 128, 256, 512], adaptive=True, k=50)
+
+    def forward(self, data, *args, **kwargs):
+        return self.decoder(self.backbone(data), *args, **kwargs)
